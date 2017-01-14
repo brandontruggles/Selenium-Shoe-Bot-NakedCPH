c1c15e8ff530c40d2c2176741cc3e6f2ea98d77f
Brandon Ruggles <brandonrninefive@gmail.com>
2017-01-14 16:12:21 -0500

    The script now uses the PhantomJS headless webdriver by default, and it also uses XPATHs a little more efficiently. A timer has also been added to show the total elapsed script time.

--- autocheckout.py
+++ autocheckout.py
@@ -3,6 +3,7 @@
 ##################################################################################################################################################################
 
 import json
+import time
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
@@ -12,7 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 confFile = None
 orders = []
-debug = True
+debug = False
 
 try:
 	#We read our JSON data from a file called 'checkout.conf' for simplicity and readability.
@@ -26,8 +27,9 @@ except Exception as e:
 def login(username, password):
 	if(username != '' and password != ''):
 		driver.get('http://www.nakedcph.com/login')
-		usernameBox = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[@id="login-form-li-email"]/input')
-		passwordBox = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[@id="login-form-li-password"]/input')
+		formUl = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul')
+		usernameBox = formUl.find_element_by_xpath('./li[@id="login-form-li-email"]/input')
+		passwordBox = formUl.find_element_by_xpath('./li[@id="login-form-li-password"]/input')
 		usernameBox.send_keys(username)
 		passwordBox.send_keys(password)
 		passwordBox.send_keys(Keys.ENTER)
@@ -47,37 +49,52 @@ def autoCompleteCaptcha():
 			print 'Error Details: ' + str(e)
 	
 def addToCart(product):
-		driver.get(product['url'])
-		autoCompleteCaptcha()
-		sizeSelection = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/div[@id="commodity-show-right"]/div/form/select[@id="commodity-show-form-size"]/option[text()="'+ product['size']  + '"]')
-		sizeSelection.click()
-		addButton = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/div[@id="commodity-show-right"]/div/a')
-		addButton.click()
+	driver.get(product['url'])
+	autoCompleteCaptcha()
+	commodityDiv = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/div[@id="commodity-show-right"]/div')
+	sizeSelection = commodityDiv.find_element_by_xpath('./form/select[@id="commodity-show-form-size"]/option[text()="'+ product['size']  + '"]')
+	sizeSelection.click()
+	addButton = commodityDiv.find_element_by_xpath('./a')
+	addButton.click()
 
 def checkout(card_number, card_expiration, card_crn):
 	cartButton = driver.find_element_by_xpath('/html/body/div[@id="header-container"]/div/div[@id="main-cart"]/a')
 	cartButton.click()
-	proceedButton = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/a[2]')
+	mainContentDiv = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div')
+	proceedButton = mainContentDiv.find_element_by_xpath('./a[2]')
 	proceedButton.click()
-	emailBox = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[@id="details-form-li-email"]/input')
-	emailBox2 = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[@id="details-form-li-email_repeat"]/input')
-	checkoutButton = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[15]/input[@id="details-form-submit"]')
-	
+	mainContentDiv = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div')
+
+	##########Try this code if the code in the box below doesn't work##########################################
+	#emailBox = mainContentDiv.find_element_by_xpath('./form/ul/li[@id="details-form-li-email"]/input')
+	#emailBox2 = mainContentDiv.find_element_by_xpath('./form/ul/li[@id="details-form-li-email_repeat"]/input')
+	#checkoutButton = mainContentDiv.find_element_by_xpath('./form/ul/li[15]/input[@id="details-form-submit"]')
+
+
+	##########Default Code#####################################################################################
+	emailBox = mainContentDiv.find_element_by_xpath('./form/ul/li[@id="details-form-li-email"]/input')
+	emailBox2 = mainContentDiv.find_element_by_xpath('./form/ul/li[@id="details-form-li-email_repeat"]/input')
+	checkoutButton = mainContentDiv.find_element_by_xpath('./form/ul/li[15]/input[@id="details-form-submit"]')	
+	###########################################################################################################
+
 	#We can take some shortcuts since we're logged in...
 	emailBox2.send_keys(emailBox.get_attribute('value'))
-
 	checkoutButton.click()
-	checkoutButton2 = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[3]/input[@id="handling-form-submit"]')
-	checkoutButton2.click()
 	
-	totalTableBody = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/table/tbody')
-	tableItems = totalTableBody.find_elements_by_xpath('/tr')	
-	totalTableFooter = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/table/tfoot')
-	tableTotalsItems = totalTableFooter.find_elements_by_xpath('/tr')
+	mainContentDiv = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div') 
+	checkoutButton2 = mainContentDiv.find_element_by_xpath('./form/ul/li[3]/input[@id="handling-form-submit"]')
+	checkoutButton2.click()
+
+	mainContentDiv = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div') 
+	totalTable = mainContentDiv.find_element_by_xpath('./table')
+	totalTableBody = totalTable.find_element_by_xpath('./tbody')
+	tableItems = totalTableBody.find_elements_by_xpath('./tr')	
+	totalTableFooter = totalTable.find_element_by_xpath('./tfoot')
+	tableTotalsItems = totalTableFooter.find_elements_by_xpath('./tr')
 	print 'Checkout page details: '
 	print ''
 	for item in tableItems:
-		tableItemDetails = item.find_elements_by_xpath('//td')
+		tableItemDetails = item.find_elements_by_xpath('./td')
 		printableDetails = ""
 		print 'Item:'
 		for detail in tableItemDetails:
@@ -87,24 +104,24 @@ def checkout(card_number, card_expiration, card_crn):
 	print 'Totals: '
 	print ''
 	for item in tableTotalsItems:
-		tableTotalsItemName = item.find_element_by_xpath('//td[@class="title"]')
-		tableTotalsItemValue = item.find_element_by_xpath('//td[@class="total"]')
+		tableTotalsItemName = item.find_element_by_xpath('./td[@class="title"]')
+		tableTotalsItemValue = item.find_element_by_xpath('./td[@class="total"]')
 		print tableTotalsItemName.text + ':' + tableTotalsItemValue.text
 		print ''
 	
-	agreeBox = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[@id="confirm-form-li-confirmed"]/input')
+	agreeBox = mainContentDiv.find_element_by_xpath('./form/ul/li[@id="confirm-form-li-confirmed"]/input')
 
 	agreeBox.click()
 
-	checkoutButton3 = driver.find_element_by_xpath('/html/body/div[@id="main"]/div[@id="main-content"]/div/form/ul/li[2]/input[2]')
+	checkoutButton3 = mainContentDiv.find_element_by_xpath('./form/ul/li[2]/input[2]')
 	
 	checkoutButton3.click()
-
-	cardBox = driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form/dl/dd[1]/input')
-	monthBox = driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form/dl/dd[2]/input[1]')
-	yearBox = driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form/dl/dd[2]/input[2]')
-	crnBox = driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form/dl/dd[3]/input')
-	completeButton = driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form/input[2]')
+	paymentForm =  driver.find_element_by_xpath('/html/body/div/div[@id="content"]/div[@id="payment_details"]/form')
+	cardBox = paymentForm.find_element_by_xpath('./dl/dd[1]/input')
+	monthBox = paymentForm.find_element_by_xpath('./dl/dd[2]/input[1]')
+	yearBox = paymentForm.find_element_by_xpath('./dl/dd[2]/input[2]')
+	crnBox = paymentForm.find_element_by_xpath('./dl/dd[3]/input')
+	completeButton = paymentForm.find_element_by_xpath('./input[2]')
 	cardBox.send_keys(card_number)
 	monthBox.send_keys(card_expiration[0:2])
 	yearBox.send_keys(card_expiration[3:])
@@ -116,10 +133,13 @@ def checkout(card_number, card_expiration, card_crn):
 	print ''
 
 orderIndex = 1
+startTime = time.time()
 for order in orders:
 	print 'Processing order number ' + str(orderIndex) + '...'
 	print ''
-	driver = webdriver.Chrome()
+	#driver = webdriver.Chrome() #Non-headless webdriver
+	driver = webdriver.PhantomJS() #Headless webdriver
+	
 	logged_in = login(order['username'], order['password'])
 	if(logged_in):
 		for product in order['products']:
@@ -139,4 +159,4 @@ for order in orders:
 	else:
 		print 'No username and/or password provided. The order cannot be processed.'
 	orderIndex+=1
-
+print 'Total elapsed script time: ' + str(time.time() - startTime) + ' second(s).'
